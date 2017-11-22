780394cd51ab86979bf261e334579893f678e83d
Hulsken Alexandre <hulsken@a16p16.fil.univ-lille1.fr>
2017-11-22 11:15:06 +0100

    implement timer (ex1)

--- src/main/java/fil/coo/App.java
+++ src/main/java/fil/coo/App.java
@@ -1,13 +1,31 @@
 package fil.coo;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Timer;
+
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+	public static void main( String[] args )    {
+		int delay = 1000;
+		ActionListener task = new App.ActionListenerTest();
+		Timer timer = new Timer(delay, task);
+		timer.start();
+
+		while (true);
     }
+
+	public static class ActionListenerTest implements ActionListener {
+
+		public void actionPerformed(ActionEvent e) {
+			System.out.println(java.util.Calendar.getInstance().getTime());
+		}
+		
+	}
 }
